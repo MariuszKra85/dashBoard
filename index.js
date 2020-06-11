61b0989c387375edaa6bc1a4995d42bdd478cbc5
Mariusz Krawczyk <przyslony@gmail.com>
2020-06-11 11:58:10 +0100

    create header and button to change mode.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello');
+const modeButton = document.querySelector('#modeButton');
+let mode = 'Dark';
+
+const init = () => {
+  console.log('odpala');
+
+  const header = document.querySelector(`.header--container`);
+  const modeSlider = document.querySelector(`.mode--slider-dot`);
+  const modeName = document.querySelector(`#modeName`);
+
+  modeButton.addEventListener('click', () => {
+    header.classList.toggle('light');
+    header.classList.toggle('dark');
+
+    header.classList.value.includes('dark')
+      ? (mode = 'Dark')
+      : (mode = 'Light');
+
+    modeSlider.classList.toggle(`switch-mode`);
+    modeName.textContent = `${mode} mode`;
+  });
+};
+init();
