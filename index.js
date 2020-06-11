e457df9e2404c97a3574a98139002d200c2c4e60
Mariusz Krawczyk <przyslony@gmail.com>
2020-06-11 13:09:15 +0100

    create followers items

--- index.js
+++ index.js
@@ -4,17 +4,15 @@ let mode = 'Dark';
 const init = () => {
   console.log('odpala');
 
-  const header = document.querySelector(`.header--container`);
+  const side = document.querySelector(`body`);
   const modeSlider = document.querySelector(`.mode--slider-dot`);
   const modeName = document.querySelector(`#modeName`);
 
   modeButton.addEventListener('click', () => {
-    header.classList.toggle('light');
-    header.classList.toggle('dark');
+    side.classList.toggle('light');
+    side.classList.toggle('dark');
 
-    header.classList.value.includes('dark')
-      ? (mode = 'Dark')
-      : (mode = 'Light');
+    side.classList.value.includes('dark') ? (mode = 'Dark') : (mode = 'Light');
 
     modeSlider.classList.toggle(`switch-mode`);
     modeName.textContent = `${mode} mode`;
