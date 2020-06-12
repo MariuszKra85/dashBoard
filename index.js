fe04d7ad05c4ce0f0ea8f178f67eeff93015b1d6
Mariusz Krawczyk <przyslony@gmail.com>
2020-06-12 07:28:13 +0100

    create overview and boxes, js add function to change toggle mode

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const modeButton = document.querySelector('#modeButton');
 let mode = 'Dark';
 
 const init = () => {
-  console.log('odpala');
-
   const side = document.querySelector(`body`);
   const modeSlider = document.querySelector(`.mode--slider-dot`);
   const modeName = document.querySelector(`#modeName`);
+  const cards = document.querySelectorAll(`.followers`);
+  const boxes = document.querySelectorAll(`.box`);
+  const boxTitles = document.querySelectorAll('.box--title');
+  const slider = document.querySelector('.mode--slider');
+
+  const cardsArr = Array.from(cards);
+  const boxesArr = Array.from(boxes);
+  const boxTitlesArr = Array.from(boxTitles);
+  const all = cardsArr.concat(boxesArr);
 
   modeButton.addEventListener('click', () => {
     side.classList.toggle('light');
@@ -14,7 +21,16 @@ const init = () => {
 
     side.classList.value.includes('dark') ? (mode = 'Dark') : (mode = 'Light');
 
+    all.forEach((e) => {
+      // e.style.backgroundColor = 'hsl(227, 47%, 96%)';
+      e.classList.toggle('box-light');
+    });
+    boxTitlesArr.forEach((e) => {
+      e.classList.toggle('box--title--light');
+    });
+
     modeSlider.classList.toggle(`switch-mode`);
+    slider.classList.toggle(`mode--slider-light`);
     modeName.textContent = `${mode} mode`;
   });
 };
