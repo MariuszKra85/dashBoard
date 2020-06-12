1d592df687001cab37fd64181695f7eb5b0c046f
Mariusz Krawczyk <przyslony@gmail.com>
2020-06-12 10:32:24 +0100

    change scss class combine followers and box class, js add changeTheme function.

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
 const modeButton = document.querySelector('#modeButton');
 let mode = 'Dark';
 
-const init = () => {
-  const side = document.querySelector(`body`);
-  const modeSlider = document.querySelector(`.mode--slider-dot`);
-  const modeName = document.querySelector(`#modeName`);
-  const cards = document.querySelectorAll(`.followers`);
-  const boxes = document.querySelectorAll(`.box`);
-  const boxTitles = document.querySelectorAll('.box--title');
-  const slider = document.querySelector('.mode--slider');
+const side = document.querySelector(`body`);
+const modeSlider = document.querySelector(`.mode--slider-dot`);
+const modeName = document.querySelector(`#modeName`);
 
-  const cardsArr = Array.from(cards);
-  const boxesArr = Array.from(boxes);
+const boxTitles = document.querySelectorAll('.box--title');
+const slider = document.querySelector('.mode--name');
+const headerText = document.querySelector('.header--text');
+
+const init = () => {
+  modeButton.addEventListener('click', changeTheme);
+};
+
+const changeTheme = () => {
+  const cardsArr = Array.from(document.querySelectorAll(`.followers`));
+  const boxesArr = Array.from(document.querySelectorAll(`.box`));
   const boxTitlesArr = Array.from(boxTitles);
   const all = cardsArr.concat(boxesArr);
 
-  modeButton.addEventListener('click', () => {
-    side.classList.toggle('light');
-    side.classList.toggle('dark');
+  side.classList.toggle('light');
+  side.classList.toggle('dark');
 
-    side.classList.value.includes('dark') ? (mode = 'Dark') : (mode = 'Light');
+  side.classList.value.includes('dark') ? (mode = 'Dark') : (mode = 'Light');
 
-    all.forEach((e) => {
-      // e.style.backgroundColor = 'hsl(227, 47%, 96%)';
-      e.classList.toggle('box-light');
-    });
-    boxTitlesArr.forEach((e) => {
-      e.classList.toggle('box--title--light');
-    });
-
-    modeSlider.classList.toggle(`switch-mode`);
-    slider.classList.toggle(`mode--slider-light`);
-    modeName.textContent = `${mode} mode`;
+  all.forEach((e) => {
+    // e.style.backgroundColor = 'hsl(227, 47%, 96%)';
+    e.classList.toggle('box-light');
   });
+  boxTitlesArr.forEach((e) => {
+    e.classList.toggle('box--title--light');
+  });
+
+  modeSlider.classList.toggle(`switch-mode`);
+  slider.classList.toggle('mode--name-light');
+  headerText.classList.toggle('header--text-light');
+  modeName.textContent = `${mode} mode`;
 };
 init();
